0899ad15f1323ff0b5e851a0353d2c6eb721925e
Jake Thurman <jacob@thurmans.com>
2015-06-24 00:30:51 -0400

    make the option menu not be wider than needed or than the popup it's for and fix weird spacing in the css file

--- scripts/ui-core/_OptionMenu.js
+++ scripts/ui-core/_OptionMenu.js
@@ -1,36 +1,30 @@
 define(["jquery", "helperMethods"], function ($, helpers) {
     var methods = {};
-				
-		/*
-		 * @params
-		 * 				optionLinks: ($)            Items to be appended to the menu 
-		 * 				link:        ($) {optional} The refurring link, skip to avoid positioning
-		 *
-		 * @returns: $(menu)
-		 */
+	
+	/*
+	 * @params
+	 * 				optionLinks: ($)            Items to be appended to the menu 
+	 * 				link:        ($) {optional} The refurring link, skip to avoid positioning
+	 *
+	 * @returns: $(menu)
+	 */
   	methods.create = function (optionLinks, link) {		
-  			 var el = $("<div>", { "class": "options-menu shadowed" })
-             .append(optionLinks);
-						 
-				 if (link) {
-             el.insertBefore(link)
-						     .position({
-                     my: "top center",
-                     at: "bottom center",
-                     of: link
-                 }).css("top", ""); /*Clear top prop, */
-				 }
-				 else
-				     el.appendTo(document.body);
-				 
-		     setTimeout(function () {
-  			     $(document).one("click", function () {
-                 el.remove();
-  			     });
-				 }, 0);/*Keeps jquery from firing on this click event*/
-							
-				 return el;
+  		var el = $("<div>", { "class": "options-menu shadowed" })
+			.append(optionLinks);
+
+		if (link)
+            el.insertBefore(link);
+		else
+			el.appendTo(document.body);
+
+		setTimeout(function () {
+			$(document).one("click", function () {
+				el.remove();
+			});
+		}, 0);/*Keeps jquery from firing on this click event*/
+
+		return el;
   	};
-  
+
   	return methods;
 });
