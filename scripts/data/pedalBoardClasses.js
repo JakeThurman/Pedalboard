136e7bf84fa2d34bf2f4473ca3250d94b855dba7
Jake Thurman <jacob@thurmans.com>
2015-06-28 22:52:52 -0400

    fix weird spacing in pedalboard classes

--- scripts/data/pedalBoardClasses.js
+++ scripts/data/pedalBoardClasses.js
@@ -1,7 +1,7 @@
-define(["helperMethods", "textResources"], function (helpers, resources) {
-	 var classes = {};
+define([ "helperMethods" ], function (helpers) {
+	var classes = {};
 
-	 classes.PedalBoard = function (name, pedals) {
+	classes.PedalBoard = function (name, pedals) {
 		this.Name = name;
 	 
         var thisBoard = this;
@@ -9,22 +9,22 @@ define(["helperMethods", "textResources"], function (helpers, resources) {
   			this.pedals = pedals;
     
 		this.Add = function(pedal) {
-			  thisBoard.pedals.push(pedal);
+			thisBoard.pedals.push(pedal);
 		};
 				
 		this.Remove = function(pedalId){
 			var pedalsWithThisId = helpers.where(thisBoard.pedals, function (boardPedal) {
-					return boardPedal.id === pedalId;
+				return boardPedal.id === pedalId;
 			});
 				
 			var pedalToRemove = helpers.single(pedalsWithThisId,
 				function() {// more than one result
-						//Figure out which one to remove
-						console.log("TODO: handle choosing which to delete.");
-						return pedalsWithThisId[0];
+					//Figure out which one to remove
+					console.log("TODO: handle choosing which to delete.");
+					return pedalsWithThisId[0];
 				},
 				function() {// zero results
-					throw resources.noPedalsWithThatIdMessage;
+					throw "There were no pedals with id of: " + pedalId;
 				}
 			);
 				
@@ -33,8 +33,8 @@ define(["helperMethods", "textResources"], function (helpers, resources) {
 			/* We can't use the delete keyword because it leaves a messy undefined in the array. */			
 			thisBoard.pedals = helpers.where(thisBoard.pedals, function (pedal) {
 				/* Use the already deleted bool to make sure we only delete one instance of this pedal */
-					if (deletedOne) return true;
-					deletedOne = (pedal.id === pedalToRemove.id);
+				if (deletedOne) return true;
+				deletedOne = (pedal.id === pedalToRemove.id);
 				return pedal.id !== pedalToRemove.id;
 			});
 			
@@ -47,18 +47,18 @@ define(["helperMethods", "textResources"], function (helpers, resources) {
 				
         //returns a new pedalboard without any pedals in both this PedalBoard object and a passed in PedalBoard object
         this.RemoveOverlap = function(other) {
-            if (helpers.isUndefined(other) || helpers.isUndefined(other.pedals))
+            if (!(other instanceof classes.PedalBoard))
                 throw "parameter must be another pedalboard object";
             
-						var otherBoardPedalIds = helpers.select(other.pedals, function (pedal) { 
-								return pedal.id; 
-						});	
-						
-						var onlyMyPedals = helpers.where(thisBoard.pedals, function(pedal) {
-								return otherBoardPedalIds.indexOf(pedal.id) === -1; //Where the other board does not contain this pedal
-						});
-						
-						return new classes.PedalBoard(onlyMyPedals);
+			var otherBoardPedalIds = helpers.select(other.pedals, function (pedal) { 
+				return pedal.id; 
+			});	
+			
+			var onlyMyPedals = helpers.where(thisBoard.pedals, function(pedal) {
+				return otherBoardPedalIds.indexOf(pedal.id) === -1; //Where the other board does not contain this pedal
+			});
+			
+			return new classes.PedalBoard(thisBoard.Name, onlyMyPedals);
         };
     
         //Returns the total cost of all of the pedals on this pedalboard
@@ -69,62 +69,62 @@ define(["helperMethods", "textResources"], function (helpers, resources) {
             });
             return total;
         };
-   };
+	};
 		
-		function getPedals(pedalContainerData){
-				var pedals = [];
-				helpers.forEach(pedalContainerData.pedals, function (pedalData) {
-							var pedal;
-							
-							//Create a pedal of correct class
-							if (pedalData.identifier === 2)//Is a pedal
-								  pedal = new classes.Pedal(pedalData);
-							else if (pedalData.identifier === 1) {//This is a pedal line
-							    pedalData.fullName = (pedalContainerData.fullName || pedalContainerData.name) + " " + pedalData.name;
-								  pedal = new classes.PedalLine(pedalData);
-						  }
-							else //OH NO! This is a pedal brand!
-								  throw "Nothing may contain pedal brands. They are the top level. Failed on container id = " + pedalContainerData.id;
-							
-							//Prepend the container name to the full name
-							pedal.fullName = (pedalContainerData.fullName || pedalContainerData.name) + " " + pedal.fullName;
-							pedals.push(pedal);
-				});
-				return pedals;
+	function getPedals(pedalContainerData){
+		var pedals = [];
+		helpers.forEach(pedalContainerData.pedals, function (pedalData) {
+			var pedal;
+			
+			//Create a pedal of correct class
+			if (pedalData.identifier === 2)//Is a pedal
+				pedal = new classes.Pedal(pedalData);
+			else if (pedalData.identifier === 1) {//This is a pedal line
+				pedalData.fullName = (pedalContainerData.fullName || pedalContainerData.name) + " " + pedalData.name;
+				pedal = new classes.PedalLine(pedalData);
+			}
+			else //OH NO! This is a pedal brand!
+				throw "Nothing may contain pedal brands. They are the top level. Failed on container id = " + pedalContainerData.id;
+			
+			//Prepend the container name to the full name
+			pedal.fullName = (pedalContainerData.fullName || pedalContainerData.name) + " " + pedal.fullName;
+			pedals.push(pedal);
+		});
+		return pedals;
+	}
+		
+	//Same as a PedalBrand, but can be contained by a brand.
+	classes.PedalLine = function (pedalLineData) {
+		for (var key in pedalLineData) {
+			if (key === "pedals")
+				this[key] = getPedals(pedalLineData);
+			else
+				this[key] = pedalLineData[key];
 		}
+	};
 		
-		//Same as a PedalBrand, but can be contained by a brand.
-		classes.PedalLine = function (pedalLineData) {
-				for (var key in pedalLineData) {
-						if (key === "pedals")
-						    this[key] = getPedals(pedalLineData);
-						else
-						    this[key] = pedalLineData[key];
-				}
-    };
+	classes.PedalBrand = function(pedalBrandData) {
+		for (var key in pedalBrandData) {
+			if (key === "pedals")
+				this[key] = getPedals(pedalBrandData);
+			else
+				this[key] = pedalBrandData[key];
+		}
+	};
 		
-		classes.PedalBrand = function(pedalBrandData) {
-				for (var key in pedalBrandData) {
-						if (key === "pedals")
-						    this[key] = getPedals(pedalBrandData);
-						else
-						    this[key] = pedalBrandData[key];
-				}
-		};
+	classes.Pedal = function(pedalData) {
+		for (var key in pedalData)
+			this[key] = pedalData[key];
 		
-	 classes.Pedal = function(pedalData) {
-	 			for (var key in pedalData)
-				    this[key] = pedalData[key];
-				
-				//This will be appended to by parent containers
-				this.fullName = this.name;
-    };
+		//This will be appended to by parent containers
+		this.fullName = this.name;
+	};
 		
-		//I may not use this.
-		classes.PedalType = function(pedalTypeData) {
-        this.name = pedalTypeData.name;
-				this.id = pedalTypeData.id;
-		};
-		
-	  return classes;
+	//I may not use this.
+	classes.PedalType = function(pedalTypeData) {
+	this.name = pedalTypeData.name;
+		this.id = pedalTypeData.id;
+	};
+
+	return classes;
 });
