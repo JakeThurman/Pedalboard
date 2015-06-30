9687fc3ec7ab55067e2e94851a5b1a6c67a03001
Jake Thurman <jacob@thurmans.com>
2015-06-29 22:23:57 -0400

    Use pedal colors for report color and a lighter version of it for the highlighted version

--- scripts/reporting/reportDataHelpers.js
+++ scripts/reporting/reportDataHelpers.js
@@ -3,14 +3,35 @@ define([ "helperMethods" ], function (helpers) {
 	
 	var methods = {};
 	
-	methods.getData = function(pedals, getName, getValue, color, highlight) {
+	/* function content taken from http://www.sitepoint.com/javascript-generate-lighter-darker-color/ */
+	function highlightColor(hex, luminance) {
+		/* validate hex string -> convert #fff to #ffffff */
+		hex = String(hex).replace(/[^0-9a-f]/gi, '');
+		if (hex.length < 6) {
+			hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+		}
+		luminance = luminance || 0;
+
+		/* convert to decimal and change luminosity */
+		var rgb = "#", c, i;
+		for (i = 0; i < 3; i++) {
+			c = parseInt(hex.substr(i*2,2), 16);
+			c = Math.round(Math.min(Math.max(0, c + (c * luminance)), 255)).toString(16);
+			rgb += ("00"+c).substr(c.length);
+		}
+
+		return rgb;
+	} 
+	
+	methods.getData = function(pedals, getName, getValue, getColor) {
 		var output = [];
 		helpers.forEach(pedals, function (pedal) {
+			var color = getColor(pedal);
 			output.push({
 				value: getValue(pedal),
 				label: getName(pedal),
 				color: color,
-				highlight: highlight,
+				highlight: highlightColor(color, 0.1), /*10% lighter*/
 			});
 		});
 		return output;
@@ -20,7 +41,7 @@ define([ "helperMethods" ], function (helpers) {
 		return methods.getData(pedals, 
 			function (pedal) { return pedal.name; },
 			function (pedal) { return pedal.price; }, 
-			"#F7464A", "#FF5A5E");
+			function (pedal) { return pedal.color; });
 	};
 	
 	return methods;
