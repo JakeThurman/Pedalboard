a59fa09ed510cd890b1c060dffccb3865f9eef30
Jake Thurman <JThurman@SwiftSoftware.com>
2015-07-28 16:18:36 -0400

    Add tests to async.Run

--- tests/core/asyncTests.js
+++ tests/core/asyncTests.js
@@ -38,18 +38,38 @@ define(["async"], function (async) {
 		});
 		
 		describe("run", function () {
+			var completed = false;
+			
+			var completionMs = -1;
+			
+			var argIn1 = "test";
+			var argOut1;
+			var argIn2 = [{ bigger: "test", obj: "becuase", i: "can" }, [ 1, 2, 3, ]];
+			var argOut2;
+			
 			beforeEach(function (done) {
-				done();
-			});
-		
-			it("should run the function ASAP", function () {
-				
+				var start = new Date().getTime();
+				async.run(function (arg1, arg2) {
+					var end = new Date().getTime();
+					
+					completed = true;
+					completionMs = end - start;
+					argOut1 = arg1;
+					argOut2 = arg2;
+					
+					done();
+				}, argIn1, argIn2);
 			});
 			
-			it("should give all extra arguments to the async function", function () {
-				async.run(function () {
-					expect()
-				});
+			it("should run the function by 15ms of when it was supposed to", function () {
+				expect(completed).toBe(true);
+				expect(completionMs).toBeGreaterThan(-1);
+				expect(completionMs).toBeLessThan(15);
+			});
+			
+			it("should give all extra arguments to the delayed function", function () {
+				expect(argOut1).toEqual(argIn1);
+				expect(argOut2).toEqual(argIn2);
 			});
 		});
 	});
